41d869f43d6cf35e6da262c70876690d18724a51
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-05-31 23:46:40 +0500

    Created create user test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,11 @@
+import json
+from base64 import b64encode
+from collections import OrderedDict
+from hashlib import sha256
+from hmac import HMAC
+from urllib.parse import urlencode
+
+from django.conf import settings
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -21,4 +29,27 @@ class TestWeekSchedule(APITestCase):
             'group': 32
         }
         response = self.client.get(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestUser(APITestCase):
+    def test_user_create(self):
+        vk_subset = OrderedDict([('vk_access_token_settings', 'notify'), ('vk_app_id', '7227055'),
+                                 ('vk_are_notifications_enabled', '0'), ('vk_is_app_user', '1'),
+                                 ('vk_is_favorite', '1'), ('vk_language', 'ru'),
+                                 ('vk_platform', 'desktop_web'), ('vk_ref', 'other'),
+                                 ('vk_ts', '1614515174'), ('vk_user_id', '12345')])
+        hash_code = b64encode(
+            HMAC(settings.CLIENT_SECRET_KEY.encode(), urlencode(vk_subset, doseq=True).encode(),
+                 sha256).digest())
+        sign = hash_code.decode('utf-8')[:-1].replace('+', '-').replace('/', '_')
+        vk_subset['sign'] = sign
+        vk_headers = {'HTTP_X_VK_DATA': '&'.join(
+            ['{}={}'.format(item, vk_subset[item]) for item in vk_subset])}
+        url = reverse('user')
+        data = {
+            'group': 32
+        }
+        response = self.client.post(url, data=json.dumps(data), content_type='application/json',
+                                    **vk_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ from api import views
 urlpatterns = [
     path('schedule', views.get_schedule, name='get-schedule'),
     path('week_schedule', views.get_week_schedule, name='get-week-schedule'),
-    path('user', views.UserView.as_view()),
+    path('user', views.UserView.as_view(), name='user'),
     path('startup_info', views.get_startup_info),
 ]
