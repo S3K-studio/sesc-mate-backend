4d262c77e1b699fd6dcc2eb9dc42dfc68a0663a8
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-05-31 22:49:02 +0500

    Created get_week_schedule test

--- api/tests.py
+++ api/tests.py
@@ -2,11 +2,22 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+
 class TestSchedule(APITestCase):
     def test_get_schedule(self):
         url = reverse('get-schedule')
         data = {
             'day': 1,
+            'group': 32
+        }
+        response = self.client.get(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestWeekSchedule(APITestCase):
+    def test_get_week_schedule(self):
+        url = reverse('get-week-schedule')
+        data = {
             'group': 32
         }
         response = self.client.get(url, data)
