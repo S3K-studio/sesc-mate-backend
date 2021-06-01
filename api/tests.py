9d14669611c4cdee6b287d7ab754769ca0269248
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-06-01 11:47:58 +0500

    Created user get tests

--- api/tests.py
+++ api/tests.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from api.serializers import UserSerializer
 from utils import generate_vk_headers
 
 
@@ -49,4 +50,31 @@ class CreateUserTest(APITestCase):
                                                              False)
         response = self.client.post(url, data=json.dumps(data), content_type='application/json',
                                     **vk_headers)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class GetUserTest(APITestCase):
+    def setUp(self):
+        user_data = {
+            'vk_user_id': 12345,
+            'group': 32,
+            'first_name': 'first_name',
+            'last_name': 'last_name',
+            'sex': 2,
+            'profile_picture_url': ''
+        }
+        serializer = UserSerializer()
+        serializer.create(validated_data=user_data)
+
+    def test_valid_user_get(self):
+        url = reverse('user')
+        vk_headers = generate_vk_headers.generate_vk_headers(12345, settings.CLIENT_SECRET_KEY)
+        response = self.client.get(url, data={}, **vk_headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_invalid_user_get(self):
+        url = reverse('user')
+        vk_headers = generate_vk_headers.generate_vk_headers(12345, settings.CLIENT_SECRET_KEY,
+                                                             False)
+        response = self.client.get(url, data={}, **vk_headers)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
