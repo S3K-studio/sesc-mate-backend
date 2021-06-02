4fa6e19ad06430d5267fcc3bb9885a84e8a4ebd9
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-06-02 09:41:49 +0500

    Changed tests * improved user get tests * improved user update tests * improved user delete tests

--- api/tests.py
+++ api/tests.py
@@ -69,9 +69,11 @@ class CreateUserTest(APITestCase):
 
 class GetUserTest(APITestCase):
     def setUp(self):
+        self.vk_user_id = 12345
+        self.user_group = 32
         user_data = {
-            'vk_user_id': 12345,
-            'group': 32,
+            'vk_user_id': self.vk_user_id,
+            'group': self.user_group,
             'first_name': 'first_name',
             'last_name': 'last_name',
             'sex': 2,
@@ -82,9 +84,14 @@ class GetUserTest(APITestCase):
 
     def test_valid_user_get(self):
         url = reverse('user')
+        response_valid_data = {
+            'vk_user_id': self.vk_user_id,
+            'group': self.user_group
+        }
         vk_headers = generate_vk_headers.generate_vk_headers(12345, settings.CLIENT_SECRET_KEY)
         response = self.client.get(url, data={}, **vk_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, response_valid_data)
 
     def test_invalid_user_get(self):
         url = reverse('user')
@@ -96,8 +103,10 @@ class GetUserTest(APITestCase):
 
 class UpdateUserTest(APITestCase):
     def setUp(self):
+        self.vk_user_id = 12345
+        self.user_group = 22
         user_data = {
-            'vk_user_id': 12345,
+            'vk_user_id': self.vk_user_id,
             'group': 32,
             'first_name': 'first_name',
             'last_name': 'last_name',
@@ -107,15 +116,20 @@ class UpdateUserTest(APITestCase):
         serializer = UserSerializer()
         serializer.create(validated_data=user_data)
         self.data = {
-            'group': 22
+            'group': self.user_group
         }
 
     def test_valid_user_update(self):
         url = reverse('user')
+        response_valid_data = {
+            'vk_user_id': self.vk_user_id,
+            'group': self.user_group
+        }
         vk_headers = generate_vk_headers.generate_vk_headers(12345, settings.CLIENT_SECRET_KEY)
         response = self.client.put(url, data=json.dumps(self.data), content_type='application/json',
                                    **vk_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, response_valid_data)
 
     def test_invalid_user_update(self):
         url = reverse('user')
@@ -128,9 +142,11 @@ class UpdateUserTest(APITestCase):
 
 class DeleteUserTest(APITestCase):
     def setUp(self):
+        self.vk_user_id = 12345
+        self.user_group = 32
         user_data = {
-            'vk_user_id': 12345,
-            'group': 32,
+            'vk_user_id': self.vk_user_id,
+            'group': self.user_group,
             'first_name': 'first_name',
             'last_name': 'last_name',
             'sex': 2,
@@ -141,9 +157,14 @@ class DeleteUserTest(APITestCase):
 
     def test_valid_user_delete(self):
         url = reverse('user')
+        response_valid_data = {
+            'vk_user_id': self.vk_user_id,
+            'group': self.user_group
+        }
         vk_headers = generate_vk_headers.generate_vk_headers(12345, settings.CLIENT_SECRET_KEY)
         response = self.client.delete(url, data={}, **vk_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, response_valid_data)
 
     def test_invalid_user_delete(self):
         url = reverse('user')
