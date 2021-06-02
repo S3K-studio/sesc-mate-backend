daa64763a69033807aa30cba126532a7f62e7cd6
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-06-02 09:31:37 +0500

    Changed create user test * added created user id checking * added created user group checking

--- api/tests.py
+++ api/tests.py
@@ -47,10 +47,13 @@ class CreateUserTest(APITestCase):
         data = {
             'group': 32
         }
-        vk_headers = generate_vk_headers.generate_vk_headers(12345, settings.CLIENT_SECRET_KEY)
+        vk_user_id = 12345
+        vk_headers = generate_vk_headers.generate_vk_headers(vk_user_id, settings.CLIENT_SECRET_KEY)
         response = self.client.post(url, data=json.dumps(data), content_type='application/json',
                                     **vk_headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['vk_user_id'], vk_user_id)
+        self.assertEqual(response.data['group'], data['group'])
 
     def test_invalid_user_create(self):
         url = reverse('user')
