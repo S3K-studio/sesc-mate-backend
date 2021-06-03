c15971fbb7ed3ff8cd9ddefddf5a2c4b1b88fd17
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-06-03 13:32:27 +0500

    Created get_schedule mock test

--- api/tests/test_get_schedule.py
+++ api/tests/test_get_schedule.py
@@ -1,6 +1,16 @@
+from typing import List, Dict
+from unittest.mock import patch
+
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+
+from utils.schedule_mock import ScheduleMock
+
+
+def get_schedule(*args) -> List[List[Dict]]:
+    schedule_mock = ScheduleMock()
+    return [schedule_mock.get_schedule_mock()]
 
 
 class GetScheduleTest(APITestCase):
@@ -8,6 +18,7 @@ class GetScheduleTest(APITestCase):
     def setUp(self) -> None:
         self.url = reverse('get-schedule')
 
+    @patch('api.views.get_parsed_schedule', get_schedule)
     def test_get_schedule(self):
         data = {
             'day': 1,
@@ -15,6 +26,7 @@ class GetScheduleTest(APITestCase):
         }
         response = self.client.get(self.url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, get_schedule())
 
     def test_get_schedule_mock(self):
         data = {
