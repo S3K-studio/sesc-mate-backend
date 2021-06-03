1da6fc4c3b88334dc4d5a9685fe18304b1588c36
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-06-03 13:47:17 +0500

    Created announcements mock

---
+++ utils/announcements_mock.py
@@ -0,0 +1,32 @@
+import json
+import os
+from typing import List, Dict
+
+
+class AnnouncementsMock:
+    schedule_mock: List[Dict]
+
+    def __init__(self):
+        self.schedule_mock = []
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(AnnouncementsMock, cls).__new__(cls)
+        return cls.instance
+
+    def get_schedule_mock(self) -> List[Dict]:
+        if self.schedule_mock:
+            return self.schedule_mock
+
+        current_dir = os.path.dirname(__file__)
+        schedule_mock_json_path = os.path.join(current_dir, 'announcements_mock.json')
+
+        with open(schedule_mock_json_path, 'r', encoding='utf-8') as schedule:
+            self.schedule_mock = json.load(schedule)
+        return self.schedule_mock
+
+
+if __name__ == '__main__':
+    first_object = AnnouncementsMock()
+    second_object = AnnouncementsMock()
+    print(first_object, second_object, first_object == second_object)
