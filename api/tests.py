14f7be1acce4517a12fcdb6e8f9ddbd7e003eb45
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-06-03 12:36:04 +0500

    Created schedule mock tests * created get_schedule_mock test * created get_week_schedule_mock test * get_startup_info_schedule_mock

--- api/tests.py
+++ api/tests.py
@@ -10,34 +10,70 @@ from utils import generate_vk_headers
 
 
 class ScheduleTest(APITestCase):
+
+    def setUp(self) -> None:
+        self.url = reverse('get-schedule')
+
     def test_get_schedule(self):
-        url = reverse('get-schedule')
         data = {
             'day': 1,
             'group': 32
         }
-        response = self.client.get(url, data)
+        response = self.client.get(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_schedule_mock(self):
+        data = {
+            'day': 1,
+            'group': 32,
+            'schedule_mock': ''
+        }
+        response = self.client.get(self.url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
 class WeekScheduleTest(APITestCase):
+
+    def setUp(self) -> None:
+        self.url = reverse('get-week-schedule')
+
     def test_get_week_schedule(self):
-        url = reverse('get-week-schedule')
         data = {
             'group': 32
         }
-        response = self.client.get(url, data)
+        response = self.client.get(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_week_schedule_mock(self):
+        data = {
+            'day': 1,
+            'group': 32,
+            'schedule_mock': ''
+        }
+        response = self.client.get(self.url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
 class GetStartupInfoTest(APITestCase):
+
+    def setUp(self) -> None:
+        self.url = reverse('startup-info')
+
     def test_get_startup_info(self):
-        url = reverse('startup-info')
         data = {
             'day': 1
         }
         vk_headers = generate_vk_headers.generate_vk_headers(12345, settings.CLIENT_SECRET_KEY)
-        response = self.client.get(url, data, **vk_headers)
+        response = self.client.get(self.url, data, **vk_headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_startup_info_schedule_mock(self):
+        data = {
+            'day': 1,
+            'schedule_mock': ''
+        }
+        vk_headers = generate_vk_headers.generate_vk_headers(12345, settings.CLIENT_SECRET_KEY)
+        response = self.client.get(self.url, data, **vk_headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
